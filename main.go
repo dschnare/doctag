2536d31860e868473db48534b660206e73bdbecb
Darren Schnare <=>
2014-12-05 12:58:10 -0500

    Add usage documentation to command docs

--- main.go
+++ main.go
@@ -5,6 +5,19 @@ Use `doctag --help` for details about each command line argument.
 This command supports standard in piping of a doctag file contents.
 If the `--output` argument is not specified then output will be 
 piped to standard out.
+
+  doctag {file path} | doctag [help|/?]
+    -help=false: Show the help message.
+    -hierarchical=false: Converts the flat doctag tree into a nested JSON object.
+    -hierarchy=false: Converts the flat doctag tree into a nested JSON object. (shorthand)
+    -output="": The output file to write to.
+    -pretty=false: Print JSON result with indentation. (shorthand)
+    -pretty-print=false: Print JSON result with indentation.
+    -tag-prefix="<{": The prefix to use for doc tags.
+    -tag-separator="/": The separator character to use for hierarchical doc tags.
+    -tag-suffix="}>": The suffix to use for doc tags.
+    -trim=false: Trim the leading and trailing whitespace from all doctag values.
+    -warn=false: Print warning messages.
 */
 package main
 
